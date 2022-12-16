de68edde8ff57ec9b4765403464212ed300a429c
Thanzeela K A <thanzeela.ka@experionglobal.local>
2022-12-16 17:19:16 +0530

    thanz

--- Server/apps/Leave/controllers/leaveController.js
+++ Server/apps/Leave/controllers/leaveController.js
@@ -1,4 +1,4 @@
-const{ companyMaster,workingDays}=require("../../../data/models")
+const { companyMaster, workingDays } = require("../../../data/models")
 const sequelize = require("sequelize")
 
 
@@ -10,64 +10,76 @@ workingDays.create({
     month: 'JAN',
     year: '2022',
     workingDays: 22,
+    days:31,
 },
-{
-    month: 'feb',
-    year: '2022',
-    workingDays: 20,
+    {
+        month: 'feb',
+        year: '2022',
+        workingDays: 20,
+        days:28,
 
-} ,
-{
-    month: 'mar',
-    year: '2022',
-    workingDays: 23,  
-} ,
-{
-    month: 'apr',
-    year: '2022',
-    workingDays: 20,  
-} ,
-{
-    month: 'may',
-    year: '2022',
-    workingDays: 23,  
-} ,
+    },
+    {
+        month: 'mar',
+        year: '2022',
+        workingDays: 23,
+        days:31,
+    },
+    {
+        month: 'apr',
+        year: '2022',
+        workingDays: 20,
+        days:30,
+    },
+    {
+        month: 'may',
+        year: '2022',
+        workingDays: 23,
+        days:31,
+    },
 
-{
-    month: 'jun',
-    year: 2022,
-    workingDays: 22,  
-} ,
-{
-    month: 'jul',
-    year: '2022',
-    workingDays: 21,  
-} ,
-{
-    month: 'aug',
-    year: '2022',
-    workingDays: 23,  
-} ,
-{
-    month: 'sep',
-    year: '2022',
-    workingDays: 21,  
-} ,
-{
-    month: 'oct',
-    year: '2022',
-    workingDays: 22,  
-} ,
-{
-    month: 'nov',
-    year: '2022',
-    workingDays: 22,  
-} ,
-{
-    month: 'dec',
-    year: '2022',
-    workingDays: 22,  
-} 
+    {
+        month: 'jun',
+        year: 2022,
+        workingDays: 22,
+        days:30,
+    },
+    {
+        month: 'jul',
+        year: '2022',
+        workingDays: 21,
+        days:31,
+    },
+    {
+        month: 'aug',
+        year: '2022',
+        workingDays: 23,
+        days:31,
+    },
+    {
+        month: 'sep',
+        year: '2022',
+        workingDays: 21,
+        days:30,
+    },
+    {
+        month: 'oct',
+        year: '2022',
+        workingDays: 22,
+        days:31,
+    },
+    {
+        month: 'nov',
+        year: '2022',
+        workingDays: 22,
+        days:30,
+    },
+    {
+        month: 'dec',
+        year: '2022',
+        workingDays: 22,
+        days:31,
+    }
 )
 
 
